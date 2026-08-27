177bab2ceb21702b1336ba9d513738e31a56793d
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-08-28 00:53:07 +0200

    Add fetchTopCryptos function to cryptoAPI.js

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,6 @@
+export async function fetchTopCryptos(limit = 100) {
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+  );
+  return response.json();
+}
